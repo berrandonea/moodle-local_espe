9ebb39ffa981f4ef5aae41e63b7c2a260afaf11f
Brice Errandonea <berrandonea@enp17.u-cergy.fr>
2018-05-14 10:13:42 +0200

    Update version number

--- version.php
+++ version.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package    local-mail
+ * @package    local-espe
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
